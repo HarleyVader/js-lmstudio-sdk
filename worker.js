7e3d810b9ba9f49708e6b9d1c050d199791f3a6b
HarleyVader <urukkai@outlook.com>
2024-07-02 21:14:45 +0200

    0.8.970

--- worker.js
+++ worker.js
@@ -2,13 +2,15 @@ const { parentPort } = require('worker_threads');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-let roleplay = false;
+let roleplay;
+
+roleplayReady = false;
 const sessionHistories = {};
 const userSessions = new Set();
 
 parentPort.on('message', (msg) => {
     if (msg.type === 'modelReady') {
-        roleplay = true;
+        roleplayReady = true;
         console.log('Model is ready to use.');
     } else if (msg.type === 'modelError') {
         console.error(msg.data);
