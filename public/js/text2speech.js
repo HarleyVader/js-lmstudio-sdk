8796b054b0708e853675bf5bc8d8d78f50ce4839
HarleyVader <urukkai@outlook.com>
2024-10-08 02:17:18 +0200

    3.2.3

--- public/js/text2speech.js
+++ public/js/text2speech.js
@@ -22,43 +22,34 @@ function arrayShift(_audioArray) {
   }
 }
 
-
-function audioLoad(_audioArray) {
+async function do_tts(_audioArray) {
+  state = false;
   document.querySelector("#message").textContent = "Synthesizing...";
-  let currentURL = arrayShift(_audioArray);
   let audio = document.querySelector("audio");
+  let currentURL = arrayShift(_audioArray);
   audio.src = currentURL;
+  console.log("audio.src ", audio.src);
   audio.load();
   audio.onloadedmetadata = function () {
     console.log("audio ", audio);
-    console.log("Audio loaded with duration: ", audio.duration);
+    console.log("audio.duration ", audio.duration);
     duration = audio.duration * 1000;
-    delayer(audio);
-    return audio;
-  };
-  audio.onerror = function () {
-    console.error("Error loading audio");
-  };
-}
-
-async function do_tts(_audioArray, audio) {
-  state = false;
-  document.querySelector("#message").textContent = "Playing...";
+    document.querySelector("#message").textContent = "Playing...";
     delayer();
-    audio.play()
+    audio.play();
+  };
   audio.onended = function () {
     console.log("audio ended");
     document.querySelector("#message").textContent = "Finished!";
-    audioLoad(_audioArray);
   };
 }
 
 
-function delayer(audio) {
+function delayer() {
   setTimeout(() => {
     console.log("Delayer: Delayed message");
-    if (_audioArray.length > 0) {
-      do_tts(_audioArray, audio);
+    if (_audioArray.length > 0 && state === true) {
+      do_tts(_audioArray);
     }
   }, duration);
   if (!state) state = true;
