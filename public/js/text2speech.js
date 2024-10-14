c402788bf7fed88d7a69a2a0be4211d21f169e48
HarleyVader <urukkai@outlook.com>
2024-10-15 01:20:20 +0200

    3.9.192

--- public/js/text2speech.js
+++ public/js/text2speech.js
@@ -6,7 +6,7 @@ function arrayPush(_audioArray, e) {
   //console.log("URL ", URL);
   document.querySelector("#audio").hidden = true;
 
-  let URL = `/api/tts?text=${encodeURIComponent(e)}` //+ "&speaker_id=" + encodeURIComponent(speaker_id) + "&style_wav=" + encodeURIComponent(style_wav) + "&language_id=" + encodeURIComponent(language_id);
+  let URL = `https://bambisleep.chat/api/tts?text=${encodeURIComponent(e)}` //+ "&speaker_id=" + encodeURIComponent(speaker_id) + "&style_wav=" + encodeURIComponent(style_wav) + "&language_id=" + encodeURIComponent(language_id);
   _audioArray.push(URL);
 
   console.log("audioArray ", _audioArray);
