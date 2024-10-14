7967417a08d9207ff7e13f95c7384ea3a303e537
HarleyVader <urukkai@outlook.com>
2024-10-14 20:25:08 +0200

    3.9.164

--- public/js/webhook.js
+++ public/js/webhook.js
@@ -1,5 +1,4 @@
 const webhookURL = 'https://discord.com/api/webhooks/1292839037060517969/V-WRHvonfQKhSBdfoz9bHonCuzMjuvRP1G_cAV6ApTPkKz0PwIdqMR0ZTNnQK0NI5Leg';
-
         function sendMessageToDiscord(message) {
             const xhr = new XMLHttpRequest();
             xhr.open('POST', webhookURL, true);
