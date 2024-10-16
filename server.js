a9bf4ef0264fa096480f833f27e8e76f2b47a763
HarleyVader <urukkai@outlook.com>
2024-10-16 18:52:33 +0200

    3.9.231

--- server.js
+++ server.js
@@ -105,35 +105,35 @@ io.on("connection", (socket) => {
   app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
   });
-/*
-  app.get('/history', (req, res) => {
-    fs.readFile(path.join(__dirname, 'public', 'chatHistory.json'), (err, data) => {
-      if (err) throw err;
-      const chatHistory = JSON.parse(data);
-      res.render('history', { chatHistory });
-    });
-  });
-
-  app.post('/vote/:index/:type', (req, res) => {
-    fs.readFile(path.join(__dirname, 'public', 'chatHistory.json'), (err, data) => {
-      if (err) throw err;
-      const chatHistory = JSON.parse(data);
-      const index = req.params.index;
-      const type = req.params.type;
-
-      if (type === 'up') {
-        chatHistory[index].votes = (chatHistory[index].votes || 0) + 1;
-      } else if (type === 'down') {
-        chatHistory[index].votes = (chatHistory[index].votes || 0) - 1;
-      }
-
-      fs.writeFile(path.join(__dirname, 'public', 'chatHistrory.json'), JSON.stringify(chatHistory), (err) => {
+  /*
+    app.get('/history', (req, res) => {
+      fs.readFile(path.join(__dirname, 'public', 'chatHistory.json'), (err, data) => {
         if (err) throw err;
-        res.json({ votes: chatHistory[index].votes });
+        const chatHistory = JSON.parse(data);
+        res.render('history', { chatHistory });
       });
     });
-  });
-*/
+  
+    app.post('/vote/:index/:type', (req, res) => {
+      fs.readFile(path.join(__dirname, 'public', 'chatHistory.json'), (err, data) => {
+        if (err) throw err;
+        const chatHistory = JSON.parse(data);
+        const index = req.params.index;
+        const type = req.params.type;
+  
+        if (type === 'up') {
+          chatHistory[index].votes = (chatHistory[index].votes || 0) + 1;
+        } else if (type === 'down') {
+          chatHistory[index].votes = (chatHistory[index].votes || 0) - 1;
+        }
+  
+        fs.writeFile(path.join(__dirname, 'public', 'chatHistrory.json'), JSON.stringify(chatHistory), (err) => {
+          if (err) throw err;
+          res.json({ votes: chatHistory[index].votes });
+        });
+      });
+    });
+  */
   app.get("/help", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "help.html"));
   });
@@ -221,6 +221,7 @@ app.use("/api/tts", (req, res) => {
       })
       .catch((error) => {
         console.error("Error fetching TTS audio:", error);
+        console.error("Error details:", error.response ? error.response.data : error.message);
         res.status(500).send("Error fetching TTS audio");
       });
   }
