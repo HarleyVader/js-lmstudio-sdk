ff7bfcffa4693641b6e4f070445fd0e385288388
HarleyVader <urukkai@outlook.com>
2024-10-18 01:27:26 +0200

    3.5.212

--- server.js
+++ server.js
@@ -288,7 +288,7 @@ rl.on("line", async (line) => {
     console.log(bambisleepChalk.success("Normal mode"));
   } else if (line === "save") {
     saveSessionHistories(Worker.socketId); 
-    console.log(bambisleepChalk.success("Session history saved to file", Worker.socketId));
+    console.log(bambisleepChalk.success("Session history saved to file", socketId));
 } else {
     console.log(bambisleepChalk.error("Invalid command! update or normal"));
   }
