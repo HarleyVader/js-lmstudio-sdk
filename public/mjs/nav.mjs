0d95ffb1ead408168448eb668d8a55c393a558b9
HarleyVader <urukkai@outlook.com>
2024-10-17 09:25:45 +0200

    3.5.0

--- public/mjs/nav.mjs
+++ public/mjs/nav.mjs
@@ -3,7 +3,7 @@ export function createNav() {
     <nav>
         <a href="/" >bambisleep.chat</a>
         <a href="/psychodelic-trigger-mania">Psychodelic Trigger Mania</a>
-        <a href="/history">History</a>
+        <!-- <a href="/history">History</a> -->
         <!-- <a href="/images">Images</a> -->
         <a href="/help">Help</a>
         <a href="https://www.likera.com/forum/mybb/Thread-bambisleep-chat-Multipurpose-Bambi-Sleep-oriented-AI-GF">Like Ra's</a>
