2dbe8168f21b21d8d9b671022f5446dab39b058e
HarleyVader <urukkai@outlook.com>
2024-10-17 14:04:18 +0200

    3.5.13

--- worker.js
+++ worker.js
@@ -74,7 +74,7 @@ async function handleMessage(userPrompt, socketId) {
     sessionHistories = await getSessionHistories(collarText, userPrompt, socketId);
 
     const response = await axios.post('http://192.168.0.178:1234/v1/chat/completions', {
-      model: "solar-10.7b-instruct-v1.0-uncensored",
+      model: "Orenguteng/Llama-3.1-8B-Lexi-Uncensored-V2-GGUF/Llama-3.1-8B-Lexi-Uncensored_V2_Q8.gguf",
       messages: [
         { role: "system", content: collarText },
         { role: "user", content: userPrompt },
